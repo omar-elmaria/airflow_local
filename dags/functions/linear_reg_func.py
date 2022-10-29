85115422133f977270eb4d2e552910625384bacf
Omar ElMaria <omar.elmaria@deliveryhero.com>
2022-10-29 12:33:27 +0200

    change all imports to become local in linear_reg_func

--- dags/functions/linear_reg_func.py
+++ dags/functions/linear_reg_func.py
@@ -1,26 +1,24 @@
-# Load the packages
-import pandas as pd
-import numpy as np
-
-# Define a function that fits a linear line through the CVR points
-def model(df, cvr_col):
-    from sklearn.linear_model import LinearRegression # Local import to speed up importing
-    data_x = df[["df_total"]].values
-    data_y = df[[cvr_col]].values
-    lm = LinearRegression()
-    lm.fit(X=data_x, y=data_y)
-    return round(float(np.squeeze(lm.coef_)), 4)
-
 def linear_reg_func(granularity): # "asa" or "vendor"
     # Local imports
     from google.cloud import bigquery
     from google.cloud import bigquery_storage
+    import pandas as pd
     
     # Instantiate the BQ variables to read and write to GCP
     client = bigquery.Client(project="logistics-data-staging-flat")
     bqstorage_client = bigquery_storage.BigQueryReadClient()
     job_config = bigquery.LoadJobConfig()
     job_config.write_disposition = bigquery.WriteDisposition.WRITE_TRUNCATE
+
+    # Define a function that fits a linear line through the CVR points
+    def model(df, cvr_col):
+        from sklearn.linear_model import LinearRegression # Local import to speed up importing
+        import numpy as np
+        data_x = df[["df_total"]].values
+        data_y = df[[cvr_col]].values
+        lm = LinearRegression()
+        lm.fit(X=data_x, y=data_y)
+        return round(float(np.squeeze(lm.coef_)), 4)
     
     if granularity == "asa":
         # Download the datasets
